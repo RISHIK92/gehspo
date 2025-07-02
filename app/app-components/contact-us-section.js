42e2323d5944cfc2ab7e1f6f8cea03f41c3db9af
Rishik <rishik3555@gmail.com>
2025-07-02 12:02:17 +0530

    added environment pages

--- app/app-components/contact-us-section.js
+++ app/app-components/contact-us-section.js
@@ -2,7 +2,12 @@
 
 import { useState } from "react";
 import { Button } from "../../components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "../../components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "../../components/ui/card";
 import { Badge } from "../../components/ui/badge";
 import { Input } from "../../components/ui/input";
 import { Textarea } from "../../components/ui/textarea";
@@ -12,7 +17,7 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "../../components/ui/select"
+} from "../../components/ui/select";
 import {
   Mail,
   Phone,
